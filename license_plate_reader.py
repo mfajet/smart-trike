b0e5567fd9ece646ad514b7dfb596603bc8e9fd8
Sheila Alemany <salem010@fiu.edu>
2018-02-03 16:34:31 -0500

    license plate and emotion reader integrated

--- license_plate_reader.py
+++ license_plate_reader.py
@@ -1,16 +1,17 @@
 import cv2
 import subprocess
 
-camera = cv2.VideoCapture(0)
+def make_lp_request():
+	camera = cv2.VideoCapture(0)
 
-count = 0
-while(True):
-	return_value, image = camera.read()
-	img_file = 'test' +str( count ) + '.jpg'
-	cv2.imwrite(img_file, image)
+	count = 0
+	while(True):
+		return_value, image = camera.read()
+		img_file = 'test' +str( count ) + '.jpg'
+		cv2.imwrite(img_file, image)
 
-	print subprocess.check_output(['alpr', img_file])
-	count+=1
-	if cv2.waitKey(1) & 0xFF == ord('x') or count==3000:
-		break
-camera.release()
+		print subprocess.check_output(['alpr', img_file])
+		count+=1
+		if cv2.waitKey(1) & 0xFF == ord('x') or count==3000:
+			break
+	camera.release()
